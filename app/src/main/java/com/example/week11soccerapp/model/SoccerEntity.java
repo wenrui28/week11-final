ea0db9d9d662e806265aa167f5a08e844dba570e
wenrui28 <Wenrui.Xing@student.lut.fi>
2026-03-30 16:15:55 +0800

    Delete app directory

--- app/src/main/java/com/example/week11soccerapp/model/SoccerEntity.java
+++
@@ -1,7 +0,0 @@
-package com.example.week11soccerapp.model;
-
-public interface SoccerEntity {
-    String getId();
-    String getName();
-    String getSearchText();
-}
--- app/src/main/java/com/example/week11soccerapp/ui/FilterableTab.java
+++
@@ -1,5 +0,0 @@
-package com.example.week11soccerapp.ui;
-
-public interface FilterableTab {
-    void applyControls(String query, int sortMode);
-}
